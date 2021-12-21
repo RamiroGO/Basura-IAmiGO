317ae3b2116435d9a491f67ca1e9b8ede4584bcf
Ramiro Javier Galindo Orozco <ramigao90@outlook.com>
2021-12-21 17:09:32 -0500

    Cargar archivos de enrutamiento y html

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const morgan = require('morgan');
+const patch = require('path');
+
+const app = express();
+
+// Configurar el Puerto, en caso de no establecerse ninguno,
+//  establecer un valor por defecto 
+const PORT = process.env.PORT || 4000;
+
+// Settings
+app.set('port', PORT);
+app.set('views', patch.join(__dirname, "./views/html")); // Ubicar la carpeta de Views donde se encuentran los HTMLs
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+// Routes
+connect_server.use(require('./routes/routes'));
+
+app.listen(PORT);
+
+module.exports = app;
---
+++ src/routes/routes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const { join } = require('path');
+
+// Ventana de Inicio de la página web
+router.get("/", (_req, res) => {
+	console.log('get_init_Not Log');
+	// Dibuja la página Principal para acceder a un usuario ya existente
+	res.render("01Presentador.html");
+});
+
+// Cargar los archivos de las views sea cual se pida.
+router.get("/:ext/:file", (req, res) => {
+	const patch_file = join(__dirname, "../../views/" + req.params.ext + "/", req.params.file);
+	res.sendFile(patch_file);
+});
+
+// Exportar Enrutador
+module.exports = router;
