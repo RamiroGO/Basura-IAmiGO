6db4a97fde56a1453450418857d9267c526049fa
Ramiro Javier Galindo Orozco <ramigao90@outlook.com>
2021-12-21 16:43:05 -0500

    Cargar archivos de enrutamiento y html

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const express = require("express");
-const app = express();
+const morgan = require('morgan');
+const patch = require('path');
 
-const port = process.env.PORT || 4000;
+const connect_server = express();
 
-app.listen(port);
-console.log(`Listen on Port ${port}`);
+// Configurar el Puerto, en caso de no establecerse ninguno,
+//  establecer un valor por defecto 
+const PORT = process.env.PORT || 4000;
+
+connect_server.listen(PORT);
+
+// Settings
+connect_server.set('port', PORT);
+connect_server.set('views', patch.join(__dirname, "./views/html")); // Ubicar la carpeta de Views donde se encuentran los HTMLs
+connect_server.engine('html', require('ejs').renderFile);
+connect_server.set('view engine', 'html');
+
+// Middlewares
+connect_server.use(morgan("dev"));
+connect_server.use(express.urlencoded({ extended: false }));
+connect_server.use(express.json());
+
+
+// Routes
+connect_server.get("/", (req, res) => {
+	res.render("01Presentador.html");
+})
+
+console.log(`Listen on Port ${PORT}`);
